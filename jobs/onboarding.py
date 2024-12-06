73b1689434ad7da30a304f6732b8ebdbf2e0101f
Brandon Minnix <bminnix@gmail.com>
2024-12-06 17:54:14 -0500

    Updating logging when creating.

--- jobs/onboarding.py
+++ jobs/onboarding.py
@@ -1,6 +1,6 @@
 from nautobot.dcim.models import Device, Location, LocationType
 from nautobot.extras.jobs import Job, FileVar, ChoiceVar
-from nautobot.core.models import Status
+from nautobot.extras.models import Status
 import csv
 import io
 
