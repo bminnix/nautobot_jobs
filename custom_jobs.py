550baa90c9fd284bbee3a084d2e1b887b37cdfaa
Brandon Minnix <bminnix@gmail.com>
2024-12-06 11:59:01 -0500

    Creating initial job file to modify and work with for testing.

---
+++ custom_jobs.py
@@ -0,0 +1,24 @@
+from nautobot.dcim.models import Device, Location, LocationType
+from nautobot.extras.jobs import Job, FileVar
+import csv
+
+class ImportWayneEnterprisesOnboardingData(Job):
+    """Import Wayne Enterprises onboarding data from a CSV file."""
+    
+    csv_file = FileVar(
+        label="CSV File",
+        description="Upload a CSV file with the relevant information.",
+        required=True,
+        help_text="The CSV file containing the Wayne Enterprises onboarding data.",
+    )
+
+    class Meta:
+        name = "Import Wayne Enterprises Onboarding Data"
+        description = "Import Wayne Enterprises onboarding data from a CSV file."
+    
+    def run(self, csv_file, **kwargs):
+        with open(csv_file, newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                print(row)
+    
