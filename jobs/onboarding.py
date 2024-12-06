689156964a8b37a4209c0d31128b044d4879edea
Brandon Minnix <bminnix@gmail.com>
2024-12-06 17:42:15 -0500

    Trying someting

--- jobs/onboarding.py
+++ jobs/onboarding.py
@@ -23,6 +23,7 @@ class ImportWayneEnterprisesOnboardingData(Job):
         description = "Import Wayne Enterprises onboarding data from a CSV file."
 
     def run(self, csv_file, import_type, **kwargs):
+        location_types = {"BR": "Branch", "DC": "Data Center"}
         csv_file_content = csv_file.read().decode("utf-8")
         csv_reader = csv.DictReader(io.StringIO(csv_file_content))
 
@@ -30,6 +31,7 @@ class ImportWayneEnterprisesOnboardingData(Job):
             self.logger.info("Preparing to import locations from CSV file.")
             for row in csv_reader:
                 try:
+                    # Extract the required keyed values from the row
                     name_type_split = row["name"].split("-")
                     city = row["city"]
                     state = row["state"]
@@ -43,16 +45,19 @@ class ImportWayneEnterprisesOnboardingData(Job):
                     continue
 
                 try:
-                    location_name = name_type_split[0]
-                    location_type = name_type_split[1]
-                    self.logger.info(f"Processing {location_name} - {location_type}")
-                except IndexError:
+                    # Assign the location name and location type
+                    loc_name = name_type_split[0]
+                    loc_type = location_types[name_type_split[1]]
+                    self.logger.info(f"Processing {loc_name} - {loc_type}")
+                except IndexError: # If the location name format is invalid
                     self.logger.error(f"Invalid location name format: {row['name']}.  Expected format is 'Location Name-Location Type'")
                     continue
-
+                except KeyError as err: # If the location type is not BR or DC
+                    self.logger.error(f"Invalid abbreviated location type, only BR or DC are allowed: {err}")
+                    continue
 
                 try:
-                    location_type = LocationType.objects.get(name=location_type)
+                    location_type = LocationType.objects.get(name=loc_type)
                 except LocationType.DoesNotExist:
                     self.logger.error(f"Location type '{location_type}' does not exist.  Please create the location type before importing data.")
                     continue
